e4881f2485c07f6dcfba6b1523bbbb6d8e2002d9
Ragnar <138352581+prateek2105@users.noreply.github.com>
2025-06-23 15:53:13 +0530

    Update activityLogMiddleware.js

--- backend/middleware/activityLogMiddleware.js
+++ backend/middleware/activityLogMiddleware.js
@@ -1,16 +1,1 @@
-const ActivityLog = require('../models/activityLogModel')
 
-exports.logActivity = (activity) => {
-  return async (req, res, next) => {
-    try {
-      const log = new ActivityLog({
-        userId: req.userId,
-        activity,
-      })
-      await log.save()
-    } catch (error) {
-      console.error('Error logging activity:', error)
-    }
-    next()
-  }
-}
